acdb7ebc9653651b39c6559e5eb5a41d048c7353
Bogdan Kazaryan <bogdankazaryan@yandex.ru>
2018-02-25 23:34:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function solveEquation(equation) {
 let a, b, c, x, y;
 let array = equation.replace(/ /g,'').replace(/\*x\^2/g,',').replace(/\*x/g,',').split(',');
-a = (+array[0] == 0) ? 1 : +array[0];
-b = (+array[1] == 0) ? 1 : +array[1];
-c = (+array[2] == 0) ? 0 : +array[2];
+a = +array[0];
+b = +array[1];
+c = +array[2];
 x = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
 y = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
 let solutions = [];
